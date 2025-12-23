34d50ee44a500e5dc7d090e18d1fb0cdf19f47d4
Matheus Wendell Dantas Bezerra <matheuswdb1703@gmail.com>
2025-12-22 22:02:02 -0300

    Mentoria e Curso

--- src/App.java
+++ src/App.java
@@ -1,5 +1,27 @@
+import java.time.LocalDate;
+
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Curso curso = new Curso();
+        curso.setTitulo("curso java");
+        curso.setDescricao("descrição curso java");
+        curso.setCargaHoraria(8);
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso js");
+        curso1.setDescricao("descrição curso js");
+        curso1.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de java");
+        mentoria.setDescricao("descrição mentoria java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso);
+        System.out.println(curso1);
+        System.out.println(mentoria);
     }
 }
